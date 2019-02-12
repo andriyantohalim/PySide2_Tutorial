ad015401c1bf15cfd8bf7b3f3f162235b5bb35a8
Andriyanto Halim <andriyanto.halim@gmail.com>
2019-02-13 01:39:35 +0800

    third commit
    Addded Tax_Calc based on https://www.pythonforengineers.com/your-first-gui-app-with-python-and-pyqt/
    Added simple ui example based on https://www.blog.pythonlibrary.org/2018/05/30/loading-ui-files-in-qt-for-python/

---
+++ MainWindow_improved.py
@@ -0,0 +1,30 @@
+import sys
+
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import QApplication, QPushButton, QLineEdit
+from PySide2.QtCore import QFile, QObject
+
+
+class Form(QObject):
+
+    def __init__(self, ui_file, parent=None):
+        super(Form, self).__init__(parent)
+        uifile = QFile(ui_file)
+        uifile.open(QFile.ReadOnly)
+
+        loader = QUiLoader()
+        self.window = loader.load(uifile)
+        # uifile.close()
+
+        self.window.pushButton.clicked.connect(self.ok_handler)
+        self.window.show()
+
+    def ok_handler(self):
+        language = 'None' if not self.window.lineEdit.text() else self.window.lineEdit.text()
+        print('Favorite language: {}'.format(language))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    form = Form('mainwindow.ui')
+    sys.exit(app.exec_())
---
+++ Tax_Calc.py
@@ -0,0 +1,39 @@
+import sys
+from PySide2.QtUiTools import QUiLoader
+from PySide2 import QtWidgets, QtCore
+
+
+class MyApp(QtWidgets.QMainWindow):
+
+    def initUI(self, ui_file):
+        uifile = QtCore.QFile(ui_file)
+        uifile.open(QtCore.QFile.ReadOnly)
+
+        loader = QUiLoader()
+        self.window = loader.load(uifile)
+        # uifile.close()
+
+        self.window.calc_tax_button.clicked.connect(self.CalculateTax)
+        self.window.setWindowTitle("Halim")
+        self.window.show()
+
+    def CalculateTax(self):
+        price = int(self.window.price_box.toPlainText())
+        tax = (self.window.tax_rate.value())
+        total_price = price + ((tax / 100) * price)
+        total_price_string = "The total price with tax is: " + str(total_price)
+        self.window.result_window.setText(total_price_string)
+
+    def __init__(self, ui_file):
+        super(MyApp, self).__init__()
+        self.initUI(ui_file)
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    myappl = MyApp("Tax_Calc.ui")
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
